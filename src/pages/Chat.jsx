9def3118ff3e7fb6197c40412e903029f09bcc99
Munisa <pottshas@student.21-school.ru>
2024-12-31 12:55:22 +0200

    chat divided to 3

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Search from "../components/Search"
+import ChatScreen from "../components/ChatScreen"
+import Profile from "../components/Profile"
 
 const Chat = () => {
-  return <div>This is chat page</div>;
+  return <div className="flex">
+    <div className="h-screen bg-[#F0F0F0] w-3/12">
+      <Search/>
+    </div>
+    <div className="h-screen bg-white w-6/12">
+      <ChatScreen/>
+    </div>
+    <div className="h-screen bg-[#F0F0F0] w-3/12">
+      <Profile/>
+    </div>
+  </div>;
 };
 
 export default Chat;
