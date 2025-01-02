68786d5b28d0a74f2d3075bd28ccf7b2c91a9c88
Fotimaa <fzikrillayeva2005@gmail.com>
2025-01-02 11:09:32 +0300

    Register updeted

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -22,33 +22,33 @@ const Register = () => {
 
   return (
     <div className="w-screen h-screen grid grid-cols-2">
-      <div className="w-full h-full flex flex-col gap-8 justify-center items-center bg-gray-50 p-10">
-      <h1 className="text-3xl font-semibold mb-6">Get Started Now!</h1>
-      <form className="flex flex-col gap-8 w-full max-w-md space-y-4" onSubmit={handleSubmit(onSubmit)}>
+      <div className="w-full h-full flex flex-col gap-9 justify-center items-center bg-gray-50 ">
+      <h1 className="text-3xl font-bold ">Get Started Now!</h1>
+      <form className="flex flex-col gap-8 w-full max-w-md " onSubmit={handleSubmit(onSubmit)}>
       <div>
-            <label htmlFor="name" className="block text-sm font-medium text-gray-700">Name</label>
+            <label htmlFor="name" className="block text-lg font-semibold text-gray-700">Name</label>
             <input
               type="text"
               id="name"
-              className="mt-4px h-[32px] block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:ring-green-500 focus:border-green-500 sm:text-sm"
+              className="mt-4px h-[36px] block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:ring-green-500 focus:border-green-500 sm:text-sm"
               placeholder="Enter your name"
             />
         </div>
         <div>
-            <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email address</label>
+            <label htmlFor="email" className="block text-lg font-semibold text-gray-700">Email address</label>
             <input
               type="email"
               id="email"
-              className="mt-4px h-[32px] block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:ring-green-500 focus:border-green-500 sm:text-sm"
+              className="mt-4px h-[36px] block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:ring-green-500 focus:border-green-500 sm:text-sm"
               placeholder="Enter your email"
             />
           </div>
           <div>
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
+            <label htmlFor="password" className="block text-lg font-semibold text-gray-700">Password</label>
             <input
               type="password"
               id="password"
-              className="mt-4px h-[32px] block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:ring-green-500 focus:border-green-500 sm:text-sm"
+              className="mt-4px h-[36px] block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:ring-green-500 focus:border-green-500 sm:text-sm"
               placeholder="Enter your password"
             />
           </div>
@@ -56,22 +56,26 @@ const Register = () => {
             <input
               type="checkbox"
               id="terms"
-              className="h-4 w-4 text-green-600 focus:ring-green-500 border-gray-300 rounded"
+              className="h-5 w-5 "
             />
-            <label htmlFor="terms" className="ml-2 text-sm text-gray-700">I agree to the terms & policy</label>
+            <label htmlFor="terms" className="ml-2 text-lg text-gray-700">I agree to the terms & policy</label>
           </div>
           <button
             type="submit"
-            className="w-full bg-green-600 text-white py-2 px-4 rounded-md hover:bg-green-700 focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:outline-none"
+            className="w-full bg-green-800 text-white p-2 rounded-lg hover:bg-green-700 focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:outline-none"
           >
             Signup
           </button>
       </form>
+      <p className="text-gray-400">
+          _____________________<b className="text-black">Or</b>
+          _____________________
+        </p>
       <div className="grid grid-cols-2 gap-5">
-          <Link to={"/register"} className="flex gap-2 border px-3 p-2 rounded-lg">
+          <Link to={"/register"} className="flex gap-2 border px-3 p-2 rounded-lg hover:bg-slate-400">
             <img src="/media/googleic.png" alt="icon"/> Sign in with Google
           </Link>
-          <Link to={"/register"} className="flex gap-2 border px-3 p-2 rounded-lg">
+          <Link to={"/register"} className="flex gap-2 border px-3 p-2 rounded-lg hover:bg-slate-400">
             <img src="/media/appleicon.png" alt="icon"/> Sign in with Apple
           </Link>
       </div>
